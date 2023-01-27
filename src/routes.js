42d807ce775233e6ceb784bf2e600e5d4adc78f7
jpsecundino <jp.secundino@usp.br>
2023-01-27 15:56:37 -0300

    Add product filtering functionality

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,27 @@ const routes = express.Router();
 
 routes.get('/products', async (req, res)=>{
     const products = await getProducts();
-    return res.send(products);
+    const filteredProducts = filterProducts(products, req.query);
+    return res.send(filteredProducts);
 })
 
 async function getProducts(){
     let resp = await axios.get("https://api.beuni.com.br/atlas/brands/v2/products?q=&category=&min=0&max=99999&sortBy=featured&page=1&perPage=50")
-    return resp['data'];
+    return resp['data']['products'];
+}
+
+function filterProducts(products, query) {
+    return filterByName(query.name, products);
+}
+
+function filterByName(name, products) {
+    if (!name) {
+        return products;
+    }
+
+    return products.filter(product => {
+        return product.name.toLowerCase().startsWith(name.toLowerCase());
+    })
 }
 
 module.exports = routes;
