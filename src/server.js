9b7c4a4bd353a17956fc7af00ead1fad8d6b1e39
jpsecundino <jp.secundino@usp.br>
2023-01-29 13:43:34 -0300

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const routes = require('./routes')
-
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT)
 })
